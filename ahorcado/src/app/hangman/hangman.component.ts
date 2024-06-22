f79d119b439409f976a3639e0b79ff3f38e05603
Anthony <acahuib@unsa.edu.pe>
2024-06-22 14:04:31 -0500

    Se agrega las funciones para verificar la palabra y ver si se gano

--- ahorcado/src/app/hangman/hangman.component.ts
+++ ahorcado/src/app/hangman/hangman.component.ts
@@ -1,28 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hangman',
-  standalone: true,
-  imports: [],
   templateUrl: './hangman.component.html',
-  styleUrl: './hangman.component.css'
+  styleUrls: ['./hangman.component.css']
 })
 export class HangmanComponent implements OnInit {
-	palabras: string[] = ['angular', 'typescript', 'javascript', 'programacion', 'desarrollo'];
-  	palabra: string = '';
-  	palabraOculta: string = '';
-  	intentos: number = 0;
-  	letrasErroneas: string[] = [];
-  	juegoTerminado: boolean = false;
-  	gano: boolean = false;
-constructor() {
+
+  palabras: string[] = ['angular', 'typescript', 'javascript', 'programacion', 'desarrollo'];
+  palabra: string = '';
+  palabraOculta: string = '';
+  intentos: number = 0;
+  letrasErroneas: string[] = [];
+  juegoTerminado: boolean = false;
+  gano: boolean = false;
+
+  constructor() {
     this.nuevaPalabra();
   }
 
-ngOnInit(): void {
+  ngOnInit(): void {
   }
 
-nuevaPalabra() {
+  nuevaPalabra() {
     this.palabra = this.palabras[Math.floor(Math.random() * this.palabras.length)];
     this.palabraOculta = '_ '.repeat(this.palabra.length).trim();
     this.intentos = 0;
@@ -31,5 +31,45 @@ nuevaPalabra() {
     this.gano = false;
   }
 
+  verificar(letra: string) {
+    if (this.juegoTerminado) return;
 
+    if (this.palabra.indexOf(letra) >= 0) {
+      const palabraOcultaArr = this.palabraOculta.split(' ');
+
+      for (let i = 0; i < this.palabra.length; i++) {
+        if (this.palabra[i] === letra) {
+          palabraOcultaArr[i] = letra;
+        }
+      }
+
+      this.palabraOculta = palabraOcultaArr.join(' ');
+      this.verificarGane();
+    } else {
+      this.letrasErroneas.push(letra);
+      this.intentos++;
+      this.verificarPerdido();
+    }
+  }
+
+  verificarGane() {
+    const palabraArr = this.palabraOculta.split(' ');
+    const palabraEvaluar = palabraArr.join('');
+
+    if (palabraEvaluar === this.palabra) {
+      this.gano = true;
+      this.juegoTerminado = true;
+    }
+  }
+
+  verificarPerdido() {
+    if (this.intentos >= 6) {
+      this.juegoTerminado = true;
+    }
+  }
+
+  resetJuego() {
+    this.nuevaPalabra();
+  }
 }
+
